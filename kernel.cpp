3e0edabf3e070f707595578464ffc28b18e3b675
leo <s951706.gav6399@gmail.com>
2013-01-01 08:11:25 +0800

    11_16

--- kernel.cpp
+++ kernel.cpp
@@ -1,7 +1,15 @@
-void kernelMain(){
-  printf("Hello world!!!");
-  while(1);  
+void printf(const char* str){
+	unsigned short* VideoMemory = (unsigned short*)0xb8000;
+	for(int i = 0;str[i] !='\0';i++) {
+		*(VideoMemory+i)=(*(VideoMemory+i)&0xff)|str[i];
+	} 
 
+
+}
+
+extern "C" void kernelMain(void* multiboot_structure,unsigned int magicnumber){
+    printf("Hello world!!!");
+    while(1);  
 }
 
 
